8c763f5b30d6024856ab8a43e3618139b0f6e8b0
Benjamin Read <benread310123@gmail.com>
2026-07-18 11:41:46 +1000

    Enter any three companies.

--- 3dimensions.py
+++ 3dimensions.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 import yfinance as yf
 import scipy
@@ -15,16 +14,21 @@ returnsArrayHold = []
 returnsArrayT = []
 returnsArrayGaussian = []
 
-cola = yf.Ticker("KO")
-pepsi = yf.Ticker("PEP")
-mnst = yf.Ticker("MNST")
-v = 3 #manually set v here, or use the function to estimate v
-time = "10y"
-percentSplit = 0.7
-sensitivity = 0.25
+tickerX = input("Ticker X: ").strip().upper()
+tickerY = input("Ticker Y: ").strip().upper()
+tickerZ = input("Ticker Z: ").strip().upper()
 
-#tidy pls
-def trainingAndRealData(data,df=v):
+companyX = yf.Ticker(tickerX)
+companyY = yf.Ticker(tickerY)
+companyZ = yf.Ticker(tickerZ)
+
+v = float(input("Degrees of freedom: "))
+time = input("Time period (10y, 5y, 1y, 6mo, 3mo): ")
+percentSplit = float(input("Training data proportion: "))
+sensitivity = float(input("Sensitivity: "))
+
+
+def trainingAndRealData(data):
     df = data.history(period=time, auto_adjust = True)
     df = df.dropna()
     splitPoint = int(len(df)*percentSplit)
@@ -51,12 +55,9 @@ def trainingAndRealData(data,df=v):
     realData["T Score"] = scipy.stats.t.ppf(realData.loc[:, "Percentile"],v)
     return trainingData, realData 
     
-trainingCola, realCola = trainingAndRealData(cola)
-trainingPepsi, realPepsi= trainingAndRealData(pepsi)
-trainingMnst, realMnst = trainingAndRealData(mnst)
-
-print(trainingCola)
-print(realCola)
+trainingX, realX = trainingAndRealData(companyX)
+trainingY, realY = trainingAndRealData(companyY)
+trainingZ, realZ = trainingAndRealData(companyZ)
 
 def correlationMatrix(dataX, dataY, dataZ, target ='X'): #update to not do manually, training data
     zx = dataX["Normal Score"]
@@ -82,13 +83,14 @@ def correlationMatrix(dataX, dataY, dataZ, target ='X'): #update to not do manua
     else:
         return Rxo, Rox
 
+
 #global
-Rvox, Rovx = correlationMatrix(trainingCola,trainingPepsi, trainingMnst, target = 'X')
-Rvoy, Rovy = correlationMatrix(trainingCola,trainingPepsi, trainingMnst, target = 'Y')
-Rvoz, Rovz = correlationMatrix(trainingCola,trainingPepsi, trainingMnst, target = 'Z')
+Rvox, Rovx = correlationMatrix(trainingX,trainingY,trainingZ, target = 'X')
+Rvoy, Rovy = correlationMatrix(trainingX,trainingY,trainingZ, target = 'Y')
+Rvoz, Rovz = correlationMatrix(trainingX,trainingY,trainingZ, target = 'Z')
 
 
-def getNormalScores(dataX, dataY, dataZ, i,  target ='X'): #real data input
+def getNormalScores(dataX, dataY, dataZ, i, target ='X'): #real data input
     zDictionary = {
         'X': dataX["Normal Score"],
         'Y': dataY["Normal Score"],
@@ -102,6 +104,7 @@ def getNormalScores(dataX, dataY, dataZ, i,  target ='X'): #real data input
     Zvo = np.stack(toStack)
     #uConditional = Rvo @ np.linalg.inv(Rov) @ np.linalg(Zvo)
     return Zvo
+
 
 def getHx(dataX, dataY, dataZ,i,Rvo, Rov, target = 'X'): #real data input, don't forget Rvo and Rov from training
     #duplicating this ffs man
@@ -117,6 +120,7 @@ def getHx(dataX, dataY, dataZ,i,Rvo, Rov, target = 'X'): #real data input, don't
     hx = scipy.stats.norm.cdf((zTarget-muConditional)/stdConditional) 
     return hx
 
+
 def handleHx(hx):
     if hx <= sensitivity:
         var = 1
@@ -125,6 +129,7 @@ def handleHx(hx):
     else:
         var = 0
     return var
+
 
 def checkPositiveAndNegative(arr):
     negative = False
@@ -138,6 +143,7 @@ def checkPositiveAndNegative(arr):
         return True
     return False
 
+
 def validatePositionVector(arr): #arr: array of hx's
     h = [handleHx(temp) for temp in arr]
     hasLong = any(x > 0 for x in h)
@@ -146,9 +152,7 @@ def validatePositionVector(arr): #arr: array of hx's
         return [0, 0, 0]
     longExposure = sum(x for x in h if x > 0)
     shortExposure = -sum(x for x in h if x < 0)
-
     balanced = []
-
     for x in h:
         if x > 0:
             balanced.append(x / longExposure)
@@ -159,31 +163,32 @@ def validatePositionVector(arr): #arr: array of hx's
 
     return balanced 
 
+
 def getPositionVectorForADay(i):
     #handle if i not proper day
-    hxx= getHx(realCola,realPepsi, realMnst, i,Rvox,Rovx, target = 'X')
-    hxy =getHx(realCola,realPepsi, realMnst, i,Rvoy,Rovy, target = 'Y')
-    hxz = getHx(realCola,realPepsi, realMnst, i,Rvoz,Rovz, target = 'Z')
+    hxx= getHx(realX,realY,realZ,i,Rvox,Rovx, target = 'X')
+    hxy =getHx(realX,realY,realZ,i,Rvoy,Rovy, target = 'Y')
+    hxz = getHx(realX,realY,realZ,i,Rvoz,Rovz, target = 'Z')
     positionVector =  validatePositionVector([hxx, hxy, hxz]) 
     return positionVector    
-
 
 
 def getReturn(data,i):
     return data["Close"][i]/data["Close"][i-1]-1    
 
+
 def getTotalReturn(wPrevious,i):     
-    colaReturn = getReturn(realCola,i)
-    pepsiReturn = getReturn(realPepsi,i)
-    mnstReturn = getReturn(realMnst,i)
-    returnArray = [colaReturn,pepsiReturn, mnstReturn]
+    xReturn = getReturn(realX,i)
+    yReturn = getReturn(realY,i)
+    zReturn = getReturn(realZ,i)
+    returnArray = [xReturn,yReturn,zReturn]
     totalReturn = np.dot(wPrevious,returnArray)
     return totalReturn 
 
 
 def strategyGaussian():
     totalReturns = 1
-    for i in range(1,len(realCola)):
+    for i in range(1,len(realX)):
         wt = getPositionVectorForADay(i-1)
         dailyReturn = getTotalReturn(wt,i)
         totalReturns *= 1+dailyReturn 
@@ -191,20 +196,15 @@ def strategyGaussian():
     return totalReturns-1
 
 
-
-
 def equalWeightBuyAndHold():
     totalReturns = 1
-
-    for i in range(1, len(realCola)):
-        colaReturn = realCola["Close"].iloc[i] / realCola["Close"].iloc[i-1] - 1
-        pepsiReturn = realPepsi["Close"].iloc[i] / realPepsi["Close"].iloc[i-1] - 1
-        monsterReturn = realMnst["Close"].iloc[i] / realMnst["Close"].iloc[i-1] - 1
-
-        portfolioReturn = (1/3)*colaReturn + (1/3)*pepsiReturn + (1/3)*monsterReturn
+    for i in range(1, len(realX)):
+        xReturn = realX["Close"].iloc[i] / realX["Close"].iloc[i-1] - 1
+        yReturn = realY["Close"].iloc[i] / realY["Close"].iloc[i-1] - 1
+        zReturn = realZ["Close"].iloc[i] / realZ["Close"].iloc[i-1] - 1
+        portfolioReturn = (1/3)*xReturn + (1/3)*yReturn + (1/3)*zReturn
         returnsArrayHold.append(portfolioReturn)
         totalReturns *= 1 + portfolioReturn
-
     return totalReturns - 1
 
 
@@ -213,37 +213,32 @@ def correlationMatrixT(dataX, dataY, dataZ, target="X"):
     tx = dataX["T Score"]
     ty = dataY["T Score"]
     tz = dataZ["T Score"]
-
     def getRho(a, b):
         tau = a.corr(b, method="kendall")
         return np.sin(np.pi * tau / 2)
-
     pxy = getRho(tx, ty)
     pxz = getRho(tx, tz)
     pyz = getRho(ty, tz)
-
     if target == "X":
         Rvo = np.array([pxy, pxz])
         Rov = np.array([
             [1, pyz],
             [pyz, 1]
         ])
-
     elif target == "Y":
         Rvo = np.array([pxy, pyz])
         Rov = np.array([
             [1, pxz],
             [pxz, 1]
         ])
-
     elif target == "Z":
         Rvo = np.array([pxz, pyz])
         Rov = np.array([
             [1, pxy],
             [pxy, 1]
         ])
-
     return Rvo, Rov
+
 
 def getTScores(dataX, dataY, dataZ, i, target="X"):
     tDictionary = {
@@ -251,20 +246,18 @@ def getTScores(dataX, dataY, dataZ, i, target="X"):
         "Y": dataY["T Score"],
         "Z": dataZ["T Score"]
     }
-
     toStack = []
-
     for temp in tDictionary:
         if temp != target:
             toStack.append(tDictionary[temp].iloc[i])
-
     Tvo = np.array(toStack)
-
     return Tvo
 
-Rtox, Rotx = correlationMatrixT(trainingCola, trainingPepsi, trainingMnst, target="X")
-Rtoy, Roty = correlationMatrixT(trainingCola, trainingPepsi, trainingMnst, target="Y")
-Rtoz, Rotz = correlationMatrixT(trainingCola, trainingPepsi, trainingMnst, target="Z")
+
+Rtox, Rotx = correlationMatrixT(trainingX, trainingY, trainingZ, target="X")
+Rtoy, Roty = correlationMatrixT(trainingX, trainingY, trainingZ, target="Y")
+Rtoz, Rotz = correlationMatrixT(trainingX, trainingY, trainingZ, target="Z")
+
 
 def getHxT(dataX, dataY, dataZ, i, Rvo, Rov, target="X"):
     tDictionary = {
@@ -272,22 +265,14 @@ def getHxT(dataX, dataY, dataZ, i, Rvo, Rov, target="X"):
         "Y": dataY["T Score"],
         "Z": dataZ["T Score"]
     }
-
     tTarget = tDictionary[target].iloc[i]
     Tvo = getTScores(dataX, dataY, dataZ, i, target=target)
-
     invRov = np.linalg.inv(Rov)
-
     q = len(Tvo)  # number of conditioning variables, so here q = 2
-
     muConditional = Rvo @ invRov @ Tvo
-
     baseVariance = 1 - Rvo @ invRov @ Rvo.T
-
     tailScale = (v + Tvo @ invRov @ Tvo) / (v + q)
-
     stdConditional = np.sqrt(tailScale * baseVariance)
-
     hx = scipy.stats.t.cdf(
         (tTarget - muConditional) / stdConditional,
         df=v + q
@@ -295,10 +280,11 @@ def getHxT(dataX, dataY, dataZ, i, Rvo, Rov, target="X"):
 
     return hx
 
+
 def strategyT():
     totalReturns = 1
 
-    for i in range(1, len(realCola)):
+    for i in range(1, len(realX)):
         wt = getPositionVectorForADayT(i-1)
 
         dailyReturn = getTotalReturn(wt, i)
@@ -307,35 +293,30 @@ def strategyT():
 
     return totalReturns - 1
 
+
 def getPositionVectorForADayT(i):
-    hxx = getHxT(realCola, realPepsi, realMnst, i, Rtox, Rotx, target="X")
-    hxy = getHxT(realCola, realPepsi, realMnst, i, Rtoy, Roty, target="Y")
-    hxz = getHxT(realCola, realPepsi, realMnst, i, Rtoz, Rotz, target="Z")
-
+    hxx = getHxT(realX, realY, realZ, i, Rtox, Rotx, target="X")
+    hxy = getHxT(realX, realY, realZ, i, Rtoy, Roty, target="Y")
+    hxz = getHxT(realX, realY, realZ, i, Rtoz, Rotz, target="Z")
     positionVector = validatePositionVector([hxx, hxy, hxz])
-
     return positionVector
+
 
 def performanceStats(name, returnsArray):
     returnsArray = np.array(returnsArray)
-
     totalReturn = np.prod(1 + returnsArray) - 1
     dailyVolatility = np.std(returnsArray)
     annualisedVolatility = dailyVolatility * np.sqrt(252)
-
     if dailyVolatility != 0:
         sharpeRatio = np.mean(returnsArray) * np.sqrt(252) / dailyVolatility
     else:
         sharpeRatio = np.nan
-
     cumulative = np.cumprod(1 + returnsArray)
     runningMax = np.maximum.accumulate(cumulative)
     drawdowns = cumulative / runningMax - 1
     maxDrawdown = np.min(drawdowns)
-
     winRate = np.mean(returnsArray > 0)
     averageDailyReturn = np.mean(returnsArray)
-
     activeDays = np.sum(returnsArray != 0)
     exposure = activeDays / len(returnsArray)
 
@@ -350,12 +331,15 @@ def performanceStats(name, returnsArray):
     print("Exposure:", exposure * 100, "%")
     print()
 
+
 def main():
-    #performanceStats('Hold', returnsArrayHold)
+    print("Testing:", tickerX, tickerY, tickerZ)
     strategyGaussian()
     strategyT()
     equalWeightBuyAndHold()
     performanceStats('Hold', returnsArrayHold)
     performanceStats('T', returnsArrayT)
     performanceStats('Gaussian', returnsArrayGaussian)
+
+
 main()
